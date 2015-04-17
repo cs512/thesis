384fe1bdb907f74b5cbe85f296fb164dd08dea64
cs512 <1152252@tongji.edu.cn>
2015-04-17 18:01:02 +0800

    add judge script

---
+++ py/judge.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+import os, commands
+import re
+
+count = 0
+patt = re.compile(r'([1-9]\d*)|(0\.)')
+
+def getNum(s):
+    if s == None:
+        return -1
+    if s == "0.":
+        return 0
+    else:
+        return int(s)
+
+for i in range(0, 1, 4):
+    s = "./main.elf --func find --sql ./db.sqlite3 --pic ./full/ukbench{0:05}.jpg".format(i)
+    output = commands.getoutput(s)
+    sortArr = []
+    for eachLine in output.split('\n'):
+        tmp = tuple(eachLine.split(':'))
+        sortArr.append(tmp)
+        sortArr.sort(key = lambda x:int(x[1]), reverse = True)
+        sortArr = sortArr[:4]
+    for path, num in sortArr:
+        m = patt.search(path)
+        num = getNum(m.group(0))
+        if( 0 <= num and num < 4):
+            count += 1
+
+f = open("res.txt", "w")
+f.write("count:{0}\n".format(count))
+

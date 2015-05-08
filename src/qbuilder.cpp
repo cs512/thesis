e7ea5cf4f7f8c00635b9e6a4d0f7dc9e8eb7fe03
cs512 <1152252@tongji.edu.cn>
2015-05-08 14:31:29 +0800

    change code word to the center of SIFT desc

--- src/qbuilder.cpp
+++ src/qbuilder.cpp
@@ -13,10 +13,20 @@
 using namespace std;
 using namespace cv;
 
-bool comp(float a,float b)
+static bool comp(float a,float b)
 {
     return a>b;
 }
+
+static int swapBlock(void * a, void * b, size_t sz)
+{
+    void *tmp = new char[sz];
+    memcpy(tmp, a, sz);
+    memcpy(a, b, sz);
+    memcpy(b, tmp, sz);
+    return 0;
+}
+
 
 QBuilder::QBuilder()
 {
@@ -66,9 +76,16 @@ vector<bitset<256>> QBuilder::p_qlizer(Mat &descriptors)
     for(auto i = 0; i < descriptors.rows; ++i)
     {
         //auto rowPtr = (float *)&(descriptors.row(i).col(0).data[0]);
-        auto rowPtr = &(descriptors.at<float>(i, 0));
+        auto tmpPtr = &(descriptors.at<float>(i, 0));
+//        auto rowPtr = &(descriptors.at<float>(i, 0));
+        float rowPtr[128] = {0};
         float sortedRow[128] = {0};
-        memcpy(sortedRow, rowPtr, 128*sizeof(float));
+        memcpy(sortedRow, tmpPtr, 128*sizeof(float));
+        memcpy(rowPtr, tmpPtr, 128*sizeof(float));
+
+        swapBlock(rowPtr, rowPtr + 5*8, 16*sizeof(float));
+        swapBlock(rowPtr + 2*8, rowPtr + 9*8, 16*sizeof(float));
+
         sort(sortedRow, sortedRow+128, comp);
         auto f1 = sortedRow[64] + sortedRow[63];
         f1 /= 2.0f;
